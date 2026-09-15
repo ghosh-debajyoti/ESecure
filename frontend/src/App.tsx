50620333ef4afe376c04650284217557a250b36f
ghosh-debajyoti <debajyotig995@gmail.com>
2026-09-15 14:13:52 +0530

    feat: add lightweight E-KAVACH loading screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,35 @@
+import React, { Suspense, useEffect } from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AtmosHomePage } from "./AtmosHomePage";
-import { BusinessMode } from "./BusinessMode";
+
+const AtmosHomePage = React.lazy(() => import("./AtmosHomePage").then(module => ({ default: module.AtmosHomePage })));
+const BusinessMode = React.lazy(() => import("./BusinessMode").then(module => ({ default: module.BusinessMode })));
+
+function GlobalLoaderController() {
+  useEffect(() => {
+    const loader = document.getElementById("ekavach-global-loader");
+    if (loader) {
+      loader.classList.add("fade-out");
+      setTimeout(() => {
+        if (loader.parentNode) {
+          loader.parentNode.removeChild(loader);
+        }
+      }, 500);
+    }
+  }, []);
+  return null;
+}
 
 export default function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<AtmosHomePage />} />
-        <Route path="/business" element={<BusinessMode />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Suspense fallback={null}>
+        <GlobalLoaderController />
+        <Routes>
+          <Route path="/" element={<AtmosHomePage />} />
+          <Route path="/business" element={<BusinessMode />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }
